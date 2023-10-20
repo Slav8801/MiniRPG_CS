cd2950450d80fbc29596910c608f0b9038a07dad
Slav <32 Britannia House York Place>
2023-10-20 21:05:48 +0100

    adding intro and splash screen

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿namespace MiniRPG_CS
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			new MiniRPGStateMachine();
-		}
-	}
-}
+﻿namespace MiniRPG_CS { class Program { static void Main(string[] args) => new MiniRPGStateMachine(); } }
